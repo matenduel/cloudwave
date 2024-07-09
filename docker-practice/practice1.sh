5c8ee96f481c1c6536c0ed0c42b4f030e169530f
matenduel <matenduel@gmail.com>
2024-07-09 20:39:36 +0900

    Update

--- docker-practice/practice1.sh
+++ docker-practice/practice1.sh
@@ -1,4 +1,4 @@
 docker run -d --name ide -p 8444:8443 \
-  -e PASSWORD=password -e DEFAULT_WORKSPACE=/code -e PUID=1000 -e PGID=1000 -e TZ="Asia/Seoul" \
-  -v ./src:/code -v /var/run/docker.sock:/var/run/docker.sock \
+  -e PASSWORD=password -e DEFAULT_WORKSPACE=/code -e PUID=0 -e PGID=1000 -e TZ="Asia/Seoul" \
+  -v ./src:/code -v /var/run/docker.sock:/var/run/docker.sock -v config:/config \
   linuxserver/code-server:4.90.3
