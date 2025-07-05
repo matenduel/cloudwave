5bba13b6d6459a40bcbb9232df8dbead31f08627
matenduel <matenduel@gmail.com>
2025-07-05 22:20:00 +0900

    Update docker_install_script.sh

--- docker-practice/docker_install_script.sh
+++ docker-practice/docker_install_script.sh
@@ -1,5 +1,6 @@
-apt-get update
-apt-get install -y ca-certificates curl
+# Add Docker's official GPG key:
+apt-get update && apt-get upgrade
+apt-get install -y ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -7,7 +8,6 @@ chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install -y docker-ce docker-ce-cli
